55f79113d7e30b66611f07868a15ccbec66c01b4
Jiayi Ma <meltinmjy@gmail.com>
2023-05-04 20:08:27 +0200

    updated TB_2 and memoryx_2

--- Resources/memory_2.vhd
+++ Resources/memory_2.vhd
@@ -1,35 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 29.04.2023 23:58:23
--- Design Name: 
--- Module Name: memory_2 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity memory_2 is
     Port ( w_en : in boolean;
