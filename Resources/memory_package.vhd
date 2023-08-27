643dc80cd6beab43595b71fe7c7fae4c7655c1e8
Jiayi Ma <meltinmjy@gmail.com>
2023-08-27 21:11:16 +0200

    added memory_rw_package

--- Resources/memory_package.vhd
+++ Resources/memory_package.vhd
@@ -6,10 +6,10 @@ use IEEE.numeric_bit.ALL;
 
 --memory initialization
 --memory getter and setter
-
+use WORK.cpu_defs_pack.all;
 
 package mem_pack is
-use WORK.cpu_defs_pack.all;
+
 
 
 function init_memory return MemType;
@@ -78,4 +78,71 @@ end set;
 
 
 end mem_pack;
+
+use WORK.cpu_defs_pack.all;
+package mem_rw_package is
+
+procedure memory_write( --pass signal changes to the components
+    constant Td: in time;
+    constant w_addr: in AddrType;
+    constant w_data: in BusDataType;
+    signal w_en: out boolean;
+    signal addr: out AddrType;
+    signal data_to_memo: out BusDataType
+);
+
+
+procedure memory_read(
+    constant Td: in time;
+    constant r_addr: in AddrType;
+    variable r_data: out BusDataType;
+    signal w_en: out boolean;
+    signal addr: out AddrType;
+    signal data_from_memo: in BusDataType
+);
+end mem_rw_package;
+
+package body mem_rw_package is
+
+procedure memory_write(
+    constant Td: in time;
+    constant w_addr: in AddrType;
+    constant w_data: in BusDataType;
+    signal w_en: out boolean;
+    signal addr: out AddrType;
+    signal data_to_memo: out BusDataType
+) is
+begin
+    assert Td > 0 ns;
+    addr <= w_addr;
+    data_to_memo <= w_data;
+    w_en <= TRUE;
+    wait for Td;
+    w_en <= FALSE;
+    wait for Td;
+end memory_write;
+
+procedure memory_read(
+    constant Td: in time;
+    constant r_addr: in AddrType;
+    variable r_data: out BusDataType;
+    signal w_en: out boolean;
+    signal addr: out AddrType;
+    signal data_from_memo: in BusDataType
+) is 
+begin
+    assert Td > 0 ns;
+    addr <= r_addr;
+    w_en <= FALSE;
+    wait for Td;
+    r_data := data_from_memo;
+    wait for Td;
+end memory_read;
+
+
+end mem_rw_package;
+
+
+
+
 --end section Jiayi Ma
